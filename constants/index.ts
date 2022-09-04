cde5b80eb236e988d0447d50a04c0d4b5c8a53df
Andrea Hernandez <andrea.ahernandez@mercadolibre.com>
2022-09-04 15:37:01 -0300

    feat: :sparkles: add constants styles and path

---
+++ constants/index.ts
@@ -0,0 +1,9 @@
+export const PATH_HOME: string = 'home';
+export const PATH_HOTELS: string = 'hotels';
+
+export const PATH_NAV: string =  'nav';
+export const PATH_FOOTER: string = 'footer';
+export const PATH_SLIDE: string = `${PATH_HOME}?path=slide`;
+export const PATH_MESSAGE: string = `${PATH_HOME}?path=message`;
+export const PATH_CAROUSEL: string = `${PATH_HOME}?path=carousel`;
+export const PATH_RESERVE: string =  `${PATH_HOTELS}/availability`;
